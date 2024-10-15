2afcebec518c946271acc5ca7aad3c1599030f44
IceK37 <45316097+IceK37@users.noreply.github.com>
2024-10-15 14:52:06 +0200

    fix + endpoint extract-pdf-text

--- hackathon-backend/app.js
+++ hackathon-backend/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const mysql = require('mysql2');
+const multer = require('multer');
+const pdfParse = require('pdf-parse');
 
 const db = mysql.createConnection({
     host: 'localhost',
@@ -16,6 +18,24 @@ db.connect((err) => {
         return;
     }
     console.log('Connecté à la base de données MySQL');
+});
+
+const upload = multer({ storage: multer.memoryStorage() });
+
+app.post('/extract-pdf-text', upload.single('file'), async (req, res) => {
+    try {
+        if (!req.file) {
+            return res.status(400).send('Aucun fichier PDF téléchargé.');
+        }
+
+        const pdfBuffer = req.file.buffer;
+        const data = await pdfParse(pdfBuffer);
+
+        res.status(200).json({ text: data.text });
+    } catch (err) {
+        console.error('Erreur lors de l\'extraction du texte du fichier PDF:', err);
+        res.status(500).send('Erreur lors de l\'extraction du texte du fichier PDF.');
+    }
 });
 
 app.use(express.json());
@@ -38,19 +58,24 @@ app.get('/pdf', (req, res) => {
 });
 
 app.post('/pdf', (req, res) => {
-    const { file_name, file_type, file_content } = req.body;
+    const { file_name, file_content } = req.body;
 
-    if (!file_name || !file_type || !file_content) {
-        return res.status(400).send('Les champs file_name, file_type et file_content sont requis.');
+    if (!file_name || !file_content) {
+        return res.status(400).send('Les champs file_name et file_content sont requis.');
     }
 
-    const query = 'INSERT INTO pdf_files (file_name, file_type, file_content) VALUES (?, ?, ?)';
-    db.query(query, [file_name, file_type, file_content], (err, results) => {
+    const query = 'INSERT INTO pdf_files (file_name, file_content) VALUES (?, ?)';
+    db.query(query, [file_name, file_content], (err, results) => {
         if (err) {
             console.error('Erreur lors de l\'insertion des données:', err);
             return res.status(500).send('Erreur lors de l\'insertion des données.');
         }
-        res.status(201).json({ id: results.insertId, name, content }); // Envoyer une réponse avec l'ID de la nouvelle leçon
+
+        res.status(201).json({
+            id: results.insertId,
+            file_name: file_name,
+            file_content: file_content
+        });
     });
 });
 
