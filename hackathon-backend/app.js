e68bb280dfbe9eac9e96cff86bb249f0f43ff317
IceK37 <45316097+IceK37@users.noreply.github.com>
2024-10-17 11:38:59 +0200

    port 3001

--- hackathon-backend/app.js
+++ hackathon-backend/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3001;
 const mysql = require("mysql2");
 const multer = require("multer");
 const pdfParse = require("pdf-parse");
